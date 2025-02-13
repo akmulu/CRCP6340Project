8d142c6032d929809f9a0ec436de689ab3df8419
akmulu <mulukutlaakhil@gmail.com>
2025-02-13 02:20:00 -0600

    added contact.js, website still needs to be configuered with css

---
+++ public/js/contact.js
@@ -0,0 +1,13 @@
+(function() {
+    "use strict";
+    
+    document
+    .querySelector("#contact-form-button")
+    .addEventListener("click", submitMail);
+    
+    function submitMail() {
+        console.log("Thank you for clicking.");
+    }
+    
+    //contact-form-button
+    })();
