d5f45349b7fda69a5564a8a63fb36b832f527449
akmulu <mulukutlaakhil@gmail.com>
2025-03-05 13:46:18 -0600

    Updated code to include more pages for projects, ejs, and the contact me page

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 import express from "express";
+import dotenv from "dotenv";
+import * as utils from "./utils/utils.js";
+dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"]
+
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
 
-app.post("/mail", (req, res) => {
-   console.log("mail button clicked")
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs", {projectArray: data});
+});
+
+app.get("/project/:id", (req,res) => {
+let id = req.params.id;
+if(id > data.length){
+throw new Error("No project with that ID");
+}
+res.render("project.ejs", {projectArray: data, which: id})
+});
+
+
+app.get("/aboutme", (req, res) => {
+    res.render("aboutme.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.post("/mail", async (req, res) => {
+    await utils
+    .sendMessage (req.body.sub, req.body.txt)
+    .then(() => {
+    res.send({result: "success" });
+    })
+    .catch(() => {
+    res.send({ result: "failure" });
+    });
+});
+
+app.use(async (err, req, res, next) => {
+    console.log(err);
+    res.render("error.ejs");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+   console.log(`Example app listening on port ${port}`);
+}); 
--- public/js/contact.js
+++ public/js/contact.js
@@ -1,13 +1,62 @@
 (function() {
     "use strict";
     
-    document
-    .querySelector("#contact-form-button")
-    .addEventListener("click", submitMail);
-    
-    function submitMail() {
-        console.log("Thank you for clicking.");
-    }
-    
-    //contact-form-button
-    })();
+
+    let form = document.querySelector('#contact-form');
+
+    document.querySelector("#send-contact").addEventListener("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        let formValid=true
+        if (!form.checkValidity()) {
+          formValid = false;    
+        }
+        form.classList.add('was-validated')
+        if(formValid){
+            sendTheEmail();
+        }
+    });
+
+function sendTheEmail() {
+    console.log("You clicked the submit button.");
+    let firstName = document.querySelector("#contact-first").value;
+    let lastName = document.querySelector("#contact-last").value;
+    let mail = document.querySelector("#contact-email-addr").value;
+    let message = document.querySelector("#contact-question").value;
+   
+    let obj = {
+        sub: "Someone submitted a contact form!",
+        txt: `${document.querySelector("#contact-first").value}
+        ${document.querySelector("#contact-last").value} 
+        sent you a message that reads
+        ${document.querySelector("#contact-question").value}. 
+        They're email address is  
+             ${document.querySelector("#contact-email-addr").value
+        }`,
+    };
+
+    fetch("/mail", {
+        method: "POST",
+        headers: {
+        "Content-type": "application/json",
+        },
+        body: JSON.stringify(obj),
+    })
+        .then((r) = r.json())
+        .then((response) => {
+        document.querySelector ("#contact-button-response").innerHTML =
+        response.result;
+        })
+        .then(() => {
+        setTimeout (() => {
+            document. querySelector ("#contact-button-response").innerHTML = "";
+         }, "5000");
+        });
+        
+    // console.log(obj);
+//     console.log("First Name: " + firstName);
+//     console.log("Last Name: " + lastName);
+//     console.log("Email: " + mail);
+//     console.log("Message: " + message);
+ }
+})();
